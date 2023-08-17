773ac4b8192a34fd116b6e0bd10a8e22c22a9d94
Mashonne <himashcareem16@gmail.com>
2023-08-17 20:57:41 +0530

    community error fixed

--- app/(root)/communities/[id]/page.tsx
+++ app/(root)/communities/[id]/page.tsx
@@ -1,4 +1,3 @@
- 
 import { currentUser } from "@clerk/nextjs";
 import Image from 'next/image';
 
@@ -9,84 +8,83 @@ import ThreadsTab from "@/components/shared/threads-tab";
 import { fetchCommunityDetails } from "@/lib/actions/community.actions";
 import UserCard from "@/components/cards/user-card";
 
-async function Page({ params } : { params : {id: string} }) {
-    const user = await currentUser();
+async function Page({ params }: { params: { id: string } }) {
+  const user = await currentUser();
+  if (!user) return null;
 
-    if (!user) return null;
+  const communityDetails = await fetchCommunityDetails(params.id);
 
-    const communityDetails = await fetchCommunityDetails(params.id)
+  return (
+    <section>
+      <ProfileHeader
+        accountId={communityDetails.createdBy.id}
+        authUserId={user.id}
+        name={communityDetails.name}
+        username={communityDetails.username}
+        imgUrl={communityDetails.image}
+        bio={communityDetails.bio}
+        type='Community'
+      />
 
+      <div className='mt-9'>
+        <Tabs defaultValue='threads' className='w-full'>
+          <TabsList className='tab'>
+            {communityTabs.map((tab) => (
+              <TabsTrigger key={tab.label} value={tab.value} className='tab'>
+                <Image
+                  src={tab.icon}
+                  alt={tab.label}
+                  width={24}
+                  height={24}
+                  className='object-contain'
+                />
+                <p className='max-sm:hidden'>{tab.label}</p>
 
-    return ( 
-        <section>
-            <ProfileHeader 
-                accountId={communityDetails.id}
-                authUserId={user.id}
-                name={communityDetails.name}
-                username={communityDetails.username}
-                imgUrl={communityDetails.image}
-                bio={communityDetails.bio} 
-                type="Community"
+                {tab.label === "Threads" && (
+                  <p className='ml-1 rounded-sm bg-light-4 px-2 py-1 !text-tiny-medium text-light-2'>
+                    {communityDetails.threads.length}
+                  </p>
+                )}
+              </TabsTrigger>
+            ))}
+          </TabsList>
+
+          <TabsContent value='threads' className='w-full text-light-1'>
+            {/* @ts-ignore */}
+            <ThreadsTab
+              currentUserId={user.id}
+              accountId={communityDetails._id}
+              accountType='Community'
             />
+          </TabsContent>
 
-            <div className="mt-9">
-                <Tabs defaultValue="threads" className="w-full">
-                    <TabsList className="tab">
-                        {communityTabs.map((tab) => (
-                            <TabsTrigger key={tab.label} value={tab.value} className='tab'>
-                               <Image
-                                  src={tab.icon}
-                                  alt={tab.label}
-                                  width={24}
-                                  height={24}
-                                  className="object-container"
-                               />
-                               <p className="max-sm:hidden">
-                                {tab.label}
-                               </p>
+          <TabsContent value='members' className='mt-9 w-full text-light-1'>
+            <section className='mt-9 flex flex-col gap-10'>
+              {communityDetails.members.map((member: any) => (
+                <UserCard
+                  key={member.id}
+                  id={member.id}
+                  name={member.name}
+                  username={member.username}
+                  imgUrl={member.image}
+                  personType='User'
+                />
+              ))}
+            </section>
+          </TabsContent>
 
-                               {tab.label === 'Threads' && (
-                                <p className="ml-1 rounded-sm bg-light-4 px-2 py-1 !text-tiny-medium text-light-2">
-                                    {communityDetails?.threads.length}
-                                </p>
-                               )}
-                            </TabsTrigger>
-                        ))}
-                    </TabsList>
-          
-                        <TabsContent value="threads" className="w-full text-light-1">
-                            <ThreadsTab
-                                currrentUserId={user.id}
-                                accountId={communityDetails._id}
-                                accountType="Community"
-                            />
-                        </TabsContent>
-                        <TabsContent value="members" className="w-full text-light-1">
-                           <section className="my-9 flex flex-col gap-10">
-                                {communityDetails?.members.map((member: any) => (
-                                    <UserCard 
-                                        key={member.id}
-                                        id={member.id}
-                                        name={member.name}
-                                        username={member.username}
-                                        imgUrl={member.image}
-                                        personType="User"
-                                    />
-                                ))}
-                           </section>
-                        </TabsContent>
-                        <TabsContent value="requests" className="w-full text-light-1">
-                            <ThreadsTab
-                                currrentUserId={user.id}
-                                accountId={communityDetails._id}
-                                accountType="Community"
-                            />
-                        </TabsContent>
-                    
-                </Tabs>
-            </div>
-        </section>
-     );
+          <TabsContent value='requests' className='w-full text-light-1'>
+            {/* @ts-ignore */}
+            <ThreadsTab
+              currentUserId={user.id}
+              accountId={communityDetails._id}
+              accountType='Community'
+            />
+          </TabsContent>
+        </Tabs>
+      </div>
+    </section>
+  );
 }
- 
+
 export default Page;
